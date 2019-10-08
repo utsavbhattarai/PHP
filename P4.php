153b719fdbe6d8a201a6019663018d53cfd835f9
Utsav Bhattarai <utsav.bhattaraicode@gmail.com>
2019-10-07 23:58:56 -0500

    added mysql connection strings

---
+++ P4.php
@@ -0,0 +1,37 @@
+<?php
+$FirstName = $_POST['FirstName'];
+$LastName  = $_POST['LastName'];
+$qp1 = $_POST['qp1'];
+$qp2 = $_POST['qp2'];
+$qp3 = $_POST['qp3'];
+$qp4 = $_POST['qp4'];
+$qp5 = $_POST['qp5'];
+$qp6 = $_POST['qp6'];
+$qp7 = $_POST['qp7'];
+$qp8 = $_POST['qp8'];
+$qp9 = $_POST['qp9'];
+$qp10 = $_POST['qp10'];
+
+if(!empty($FirstName) ||!empty($LastName))
+{
+    $host = "localhost";
+    $dbUsername = "cmps401";
+    $dbPassword = "Mycmps401db";
+    $dbName = "cmps401";
+
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+    if(mysqli_connect_error()){
+        die('Connect Error('. mysqli_connect_erroro(). ')'. mysqli_connect_error());
+    }else{
+       
+    }
+}else 
+{
+    echo "First Name and Last Name are required";
+    die();
+}
+
+
+
+?>
